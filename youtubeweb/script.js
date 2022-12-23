7f4cb334794f3d55ff98ffa1055ab717582c343c
sajusafvan <120644031+sajusafvan@users.noreply.github.com>
2022-12-23 15:58:27 +0530

    Add files via upload

---
+++ youtubeweb/script.js
@@ -0,0 +1,22 @@
+function myFunction(){
+    
+        document.getElementById('search-space').style.visibility='visible';
+        document.getElementById('yt-logo-id').style.display='none';
+        document.getElementById('icon-menu').style.visibility='hidden';
+        document.getElementById('back-btn').style.display='inline';
+    
+
+}
+function myFunction2(){
+    document.getElementById('yt-logo-id').style.display='inline';
+    document.getElementById('back-btn').style.display='none';
+    document.getElementById('search-space').style.visibility='hidden';
+    document.getElementById('icon-menu').style.visibility='visible';
+
+
+}
+/* function functionRefresh(){
+    document.getElementById('search-space').style.visibility='hidden';
+    document.getElementById('refresh-btn').style.visibility='hidden';
+    
+}*/
